ea5df78e4327c046636ff9c4bffac7a7583cf7bb
Shreyash Lata <shreyeslata@gmail.com>
2021-03-31 01:49:41 +0530

    Add HealthApp to GitHub

---
+++ app/src/main/java/com/leoshrey/healthapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.leoshrey.healthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+//    var running = false;
+//    var sensorManager: SensorManager? = null;
+    private  SensorManager mSensorManager;
+    boolean running;
+    public TextView count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) == null){
+            Toast.makeText(this, "No Step Counter Sensor!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER), SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (running){
+            //update the UI
+            count.setText(String.valueOf(sensorEvent.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
